d9040b4efc419f46b1ba4fafa9700ed5c1ccbc9e
Gus Beringer <gusberinger@gmail.com>
2022-11-28 22:16:54 -0600

    initial commit

---
+++ replace.js
@@ -0,0 +1,11 @@
+const BASE_URL = "https://www.nytimes.com/crosswords/game/daily"
+
+const newUrl = () => {
+  const date = new Date()
+  const month = date.getMonth() + 1
+  const year = date.getFullYear()
+  const day = date.getDate()
+  return `${BASE_URL}/${year}/${month}/${day}` 
+}
+
+location.replace(newUrl())
